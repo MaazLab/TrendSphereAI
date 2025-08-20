aaed3765085c277d14e230075ccb4b56ddb1dc4c
MaazRafiq <maaz.rafique.75@gmail.com>
2025-08-20 23:48:06 +0300

    Create saperate crew functions for both research and writer

--- backend/app/crews/social_article_crew.py
+++ backend/app/crews/social_article_crew.py
@@ -1,28 +1,24 @@
-# backend/app/crews/social_article_crew.py
 from __future__ import annotations
-
 from crewai import Crew
 from agents.research_agent import create_research_agent
 from tasks.research_task import create_research_task
 from agents.writer_agent import create_writer_agent
 from tasks.writer_task import create_write_task
 
-
+# Create the Research Crew
 def create_research_crew(model: str | None = None, verbose: bool = True) -> Crew:
     print("FUNC: create_research_crew")
     agent = create_research_agent(model=model, verbose=verbose)
-    print("DEBUG\t\t agent: ",agent)
+    print("DEBUG\t\t agent: ", agent)
     task = create_research_task(agent)
-    print("DEBUG\t\t task: ",task)
+    print("DEBUG\t\t task: ", task)
     return Crew(agents=[agent], tasks=[task])
 
-def create_write_crew(model: str | None = None, verbose: bool = True) -> Crew:
-    researcher = create_research_agent(model=model, verbose=verbose)
-    writer = create_writer_agent(model=model, verbose=verbose)
-
-    # Define tasks for both agents
-    research_task = create_research_task(researcher)
-    write_task = create_write_task(writer)
-
-    # Research and Writer agents interact sequentially in the Crew
-    return Crew(agents=[researcher, writer], tasks=[research_task, write_task])
+# Create the Writer Crew
+def create_writer_crew(model: str | None = None, verbose: bool = True) -> Crew:
+    print("FUNC: create_writer_crew")
+    agent = create_writer_agent(model=model, verbose=verbose)
+    print("DEBUG\t\t agent: ", agent)
+    task = create_write_task(agent)
+    print("DEBUG\t\t task: ", task)
+    return Crew(agents=[agent], tasks=[task])
